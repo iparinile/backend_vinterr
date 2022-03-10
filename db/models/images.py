989e1fb8f320baf6fb55603ae20226ec922f751b
iparinile <poselennov.il@ya.ru>
2022-03-10 09:54:41 +0500

    update

--- db/models/images.py
+++ db/models/images.py
@@ -7,6 +7,6 @@ class DBImages(BaseModel):
     __tablename__ = 'Images'
 
     id = Column(Integer, autoincrement=True, unique=True, nullable=False, primary_key=True)
-    url = Column(VARCHAR(255), unique=True, nullable=False)
+    url = Column(VARCHAR(255), nullable=False)
     variation_id = Column(Integer, ForeignKey('Variations.id', ondelete='CASCADE'), nullable=False)
     model_info = Column(VARCHAR(255), default=None)
