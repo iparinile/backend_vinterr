462bff7f8415b6d84debb709c65874428b79e3f3
iparinile <poselennov.il@ya.ru>
2022-03-23 15:24:06 +0500

    test

--- transport/sanic/endpoints/images/image.py
+++ transport/sanic/endpoints/images/image.py
@@ -1,3 +1,5 @@
+import os
+
 from sanic.request import Request
 from sanic.response import BaseHTTPResponse
 
@@ -9,4 +11,6 @@ class ImageEndpoint(BaseEndpoint):
     async def method_get(self, request: Request, body: dict, session: DBSession, img_folder: str, img_name: str,
                          *args, **kwargs) -> BaseHTTPResponse:
         image_url = f"src/img/{img_folder}/{img_name}"
+        print(os.getcwdb())
+        print(image_url)
         return await self.make_response_file(file_url=image_url)
