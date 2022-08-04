514ca08f2f9d9bd75257a7543dc83014b684c6a0
iparinile <poselennov.il@ya.ru>
2022-08-04 09:15:10 +0500

    patch

--- transport/sanic/config.py
+++ transport/sanic/config.py
@@ -9,4 +9,4 @@ class SanicConfig:
     host = os.getenv('host', 'localhost')
     port = int(os.getenv('port', 8000))
     workers = int(os.getenv('workers', 1))
-    debug = bool(os.getenv('debug', False))
+    debug = False
